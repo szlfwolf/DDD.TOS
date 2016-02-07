dd99d5b705e69f5bdcbf99048b53c65d0b3bf02e
szlfwolf <szlfowlf@outlook.com>
2016-02-07 08:11:54 +0800

    init-schema-1

---
+++ Test/DDD.TOS.Domain.Test/ContainerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DDD.TOS.Domain.Test
+{
+	[TestClass]
+	public class ContainerTest
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
